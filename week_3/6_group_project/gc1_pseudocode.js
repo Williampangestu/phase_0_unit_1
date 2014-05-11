fc191eab81ef43a82c628279370008e402ee850a
williampp <pangestu.william@gmail.com>
2014-05-11 13:34:18 -0700

    Added my solution 6

--- week_3/6_group_project/gc1_pseudocode.js
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -22,3 +22,40 @@
 // else
 // access the 'middleNum' of the array and return it
 // exit the function
+
+
+
+// Reflection: Use the reflection guidelines
+// 
+// What parts of your strategy worked? What problems did you face?
+// My strategy was writing the actual code after I'm done pseudocoding in order to make sure that it is right.
+//
+// What questions did you have while coding? What resources did you find to help you answer them?
+// What concepts are you having trouble with, or did you just figure something out? If so, what?
+// I did not have any trouble on the concept of pseudocoding. However, I have some questions about how to make a
+// good and understable pseudocode. I used https://www.khanacademy.org/computing/cs/programming/good-practices/p/pseudo-code
+// to help me write a better pseudocode.
+//
+// Did you learn any new skills or tricks?
+// I learn how to make a better and easy to understand pseudocode.
+//
+// How confident are you with each of the Learning Competencies?
+// I am quite confident with each of them. I think I did all of them right.
+//
+// Which parts of the challenge did you enjoy?
+// writing the pseudocode.
+//
+// Which parts of the challenge did you find tedious?
+// None.
+//
+// What was this experience like?
+// For me, this is a really good experience on understanding the importance of teamwork. We would not be able to finish this
+// assignment without each other. Every person has his own jobs and each of us is dependable to each other. 
+//
+// Was your group successful in passing the tests?
+// Yes, we have successfully made the code that works and passed all the tests.
+//
+// What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
+// I learn that we have to do all this step by step. Without the user stories completed, we would not be able to move
+// to pseudocoding and so on.s
+//
